52e739c4b1de2a409e28d08accc0a4825037eeb6
sunnyramagiri <ramagirisunny.uk@gmail.com>
2025-03-20 20:56:41 +0530

    2

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "nvir" {
     ami = "ami-04b4f1a9cf54c11d0"
     instance_type = "t2.micro"
-    subnet_id = "subnet-086f49bab584dc014"
+    subnet_id = ""
     key_name = "ia"
     count = "2"
 }
@@ -10,7 +10,7 @@ resource "aws_instance" "nvir" {
 resource "aws_instance" "nusoho" {
     ami = "ami-0cb91c7de36eed2cb"
     instance_type = "t2.micro"
-    subnet_id = "subnet-05c752a94ec0f8193"
+    subnet_id = ""
     count = "1"
     provider = aws.use2
 }
