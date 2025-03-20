d352997f839c52070ea03fed894835df9b5e58e6
sunnyramagiri <136982339+sunnyramagiri@users.noreply.github.com>
2025-03-20 22:30:29 +0530

    Add files via upload

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "Mat" {
-  bucket = "sunny-backend-1996" # change this
-  tags = {
-    "name" = "dev"
-  }
+resource "aws_s3_bucket" "name" {
+    bucket = "sunny664664"
+    tags = {
+      "name" = "dev"
+    }
+}
+
+resource "aws_s3_bucket_object" "mybucket" {
+    bucket = aws_s3_bucket.name.id
+    key = "myfile.txt"
+    source = "C:/Users/ramag/OneDrive/Desktop/s3bucket/myfile.txt"
 }
